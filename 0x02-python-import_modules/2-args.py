2bed59916e5f989d2d8a51b57ebd477de2281597
Yordanos Dereje <yordadereje@gmail.com>
2022-06-02 13:24:54 +0300

    22

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 if __name__ == '__main__':
     import sys
     if len(sys.argv) == 2:
-        print(f"{len(sys.argv) - 1} argument:}")
+        print("{} argument: ".format(len(sys.argv) - 1))
     else:
-        print(f"{len(sys.argv) - 1} arguments:}")
+        print("{} arguments: ".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
-        print(f"{i}: {sys.argv[i]}")
+        print("{}: {}".format(i, sus.argv[i]))
