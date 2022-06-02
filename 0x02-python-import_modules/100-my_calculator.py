835a9b51ce95dc2324fcfb81b80c2b225e1909eb
Yordanos Dereje <yordadereje@gmail.com>
2022-06-02 17:34:25 +0300

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    if len(sys.argv) == 4:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
@@ -19,4 +19,4 @@ if __name__ == "__main__":
             print("{} {} {} = {}".format(a, op, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit(0)
+            exit(1)
