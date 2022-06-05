3f4ae76ac16e2ad68224ee467fe9ca7b14a07908
Yordanos Dereje <yordadereje@gmail.com>
2022-06-05 21:05:53 +0300

    10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
+    new_list = my_list.copy()
     for i in range(my_list):
         if my_list[i] % 2 == 0:
             new_list[i] = True
