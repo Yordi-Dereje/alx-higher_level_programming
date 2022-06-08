1f873e62fb25614373c4a13584ace4522a19e04f
Yordanos Dereje <yordadereje@gmail.com>
2022-06-08 22:20:22 +0300

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return 'None'
-    max_val = max(a_dictionary.values())
-    return max_val
+    return (max(a_dictionary, key=a_dictonary.get) if a_dictionary else None)
