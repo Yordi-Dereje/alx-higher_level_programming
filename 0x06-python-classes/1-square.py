c094978092db00afc680766ba7f816b9c77c8da8
Yordanos Dereje <yordadereje@gmail.com>
2022-06-21 23:24:23 +0300

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this code contains the class square that defines a square """
+
+
+class Square:
+    """the square class"""
+    def __init__(self, size):
+        """Initializing the square"""
+        self.__size = size
