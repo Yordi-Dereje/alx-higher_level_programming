960810fbb696b79b6db4673e8cc92c8d2c9329e5
Yordanos Dereje <yordadereje@gmail.com>
2022-06-27 21:14:00 +0300

    0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This is a module for a rectangle class
+"""
+
+
+class Rectangle:
+    """Class of a rectangle"""
+
+    def __init__(self):
+        """Initialize class"""
+        pass
