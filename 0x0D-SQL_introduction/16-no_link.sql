8e3ba84a6cb5fddf656b730411fae6d7780b9d63
Yordanos Dereje <yordadereje@gmail.com>
2022-08-09 23:43:05 +0300

    16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table in your mysql server
-SELECT score, name FROM second_table WHERE CHAR_LENGHT(name) > 0 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE CHAR_LENGTH(name) > 0 ORDER BY score DESC;
