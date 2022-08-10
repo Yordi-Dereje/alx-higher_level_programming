f880900af8e02d5dcc455ab5c376c964182fbc27
Yordanos Dereje <yordadereje@gmail.com>
2022-08-10 20:17:14 +0300

    the condition

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- lists all cities in the database
 
-SELECT id, name, states.name FROM cities JOIN states ORDER BY cities.id;
+SELECT id, name, states.name FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id;
