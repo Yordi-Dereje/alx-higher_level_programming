0f36385f6bb51acab06634cb98d1bbcebd03298a
Yordanos Dereje <yordadereje@gmail.com>
2022-08-10 07:39:38 +0300

    1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
 -- create new user user_0d_1, full privilege and has password
-ALL privileges
-password user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIES BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
