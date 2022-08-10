a6882403736506d1cb0053a0165a99211188557a
Yordanos Dereje <yordadereje@gmail.com>
2022-08-10 08:01:28 +0300

    remove single quotes

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create new user user_0d_1, full privilege and has password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIES BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIES BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost WITH GRANT OPTION;
