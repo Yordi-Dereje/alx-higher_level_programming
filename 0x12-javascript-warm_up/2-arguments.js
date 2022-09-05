2d20c4ba9a1c55f39f296000a43755d0d17c0284
Yordanos Dereje <yordadereje@gmail.com>
2022-09-05 21:37:38 +0300

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const len = process.argv.length;
+let len = process.argv.length;
 if (len === 2) {
 	console.log('No argument');
 } else if (len === 3) {
 	console.log('Argument found');
-} else if (len > 3) {
+} else {
 	console.log('Arguments found');
 }
