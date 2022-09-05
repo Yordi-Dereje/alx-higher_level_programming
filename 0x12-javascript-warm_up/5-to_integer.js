1bde8173dae41ca1e535a1ebce012a588b2b2b8d
Yordanos Dereje <yordadereje@gmail.com>
2022-09-05 22:19:08 +0300

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const number = parseInt(process.argv[2]);
-if (Number.isNaN(number)) {
+const num = process.argv[2];
+if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + number);
+  console.log('My number: ' + num);
 }
